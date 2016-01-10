3f52f37a922f2c6beab66e86028a834a1cca39fb
Alex Mufatti <info@alexmufatti.it>
2016-01-10 16:49:25 +0100

    first commit

---
+++ view1/view1.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('howoldisfiammi.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope','$interval',function($scope,$interval) {
+  $scope.days = 0;
+  var birthday = new moment('2015-12-23 17:58:00','YYYY-MM-DD HH:mm:SS');
+
+  var cal = function()
+  {
+    var now = new moment();
+    var dur = moment.duration(now.diff(birthday));
+
+    $scope.months = dur.months();
+    $scope.days = dur.days();
+    $scope.hours = dur.hours();
+    $scope.minutes = dur.minutes();
+    $scope.seconds = dur.seconds();
+  };
+  cal();
+
+  $interval(function(){
+    cal();
+  },1000);
+}]);
